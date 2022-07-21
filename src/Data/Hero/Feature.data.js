8d582cdfa25068d0ad7208cd41fdfb05b668350f
Siam Ahnaf <siamahnaf198@gmail.com>
2022-07-21 12:31:52 +0600

    version@12.0.5 (Adding Some Features)

--- src/Data/Hero/Feature.data.js
+++ src/Data/Hero/Feature.data.js
@@ -1,18 +1,20 @@
 // Features Images
+import Typescript from 'Assets/features/typescript.png';
 import Next from 'Assets/features/nextjs.png';
-import NextDark from 'Assets/features/nextjs-dark.png'
+import NextDark from 'Assets/features/nextjs-dark.png';
 import React from 'Assets/features/react.png';
 import Mui from 'Assets/features/mui.png';
+import Ant from 'Assets/features/ant.png';
 import Redux from 'Assets/features/redux.png';
 import Node from 'Assets/features/node.png';
 import Express from 'Assets/features/express.png';
-import ExpressDark from 'Assets/features/express-dark.png'
+import ExpressDark from 'Assets/features/express-dark.png';
 import Graphql from 'Assets/features/graphql.png';
-import Rest from 'Assets/features/restful.png';
-import RestDark from 'Assets/features/restful-dark.png'
+import Nest from 'Assets/features/nest.png';
 import Apollo from 'Assets/features/apollo.png';
-import ApolloDark from 'Assets/features/apollo-dark.png'
-import Typescript from 'Assets/features/typescript.png';
+import Rest from 'Assets/features/restful.png';
+import RestDark from 'Assets/features/restful-dark.png';
+import ApolloDark from 'Assets/features/apollo-dark.png';
 import Mongodb from 'Assets/features/mongo.png'
 import Postgre from 'Assets/features/postgre.png';
 import Azure from 'Assets/features/azure.png';
@@ -20,9 +22,17 @@ import Aws from 'Assets/features/aws.png';
 import AwsDark from 'Assets/features/aws-dark.png'
 import Firebase from 'Assets/features/firebase.png';
 import Alicloud from 'Assets/features/alicloud.png';
+import StripeDark from 'Assets/features/stripe-dark.png';
+import Stripe from 'Assets/features/stripe.png';
+import Paypal from 'Assets/features/paypal.png';
 
 
 export default [
+    {
+        figure: 'Typescript',
+        image: Typescript,
+        background: '#2d78c728'
+    },
     {
         figure: 'Next Js',
         image: Next,
@@ -38,6 +48,11 @@ export default [
         figure: 'Material Ui',
         image: Mui,
         background: '#007FFF36'
+    },
+    {
+        figure: 'Ant Design',
+        image: Ant,
+        background: '#f95b4a1f'
     },
     {
         figure: 'React Redux',
@@ -61,10 +76,9 @@ export default [
         background: '#E535AB28'
     },
     {
-        figure: 'Restful Api',
-        image: Rest,
-        background: '#09384B28',
-        dark: RestDark
+        figure: 'Nest Js',
+        image: Nest,
+        background: '#e0234e1c'
     },
     {
         figure: 'Apollo Server',
@@ -73,9 +87,10 @@ export default [
         dark: ApolloDark
     },
     {
-        figure: 'Typescript',
-        image: Typescript,
-        background: '#2d78c728'
+        figure: 'Restful Api',
+        image: Rest,
+        background: '#09384B28',
+        dark: RestDark
     },
     {
         figure: 'Mongo Db',
@@ -107,5 +122,16 @@ export default [
         figure: 'Ali Cloud',
         image: Alicloud,
         background: '#fe690028'
+    },
+    {
+        figure: 'Stripe',
+        image: StripeDark,
+        background: '#00000015',
+        dark: Stripe
+    },
+    {
+        figure: 'Paypal',
+        image: Paypal,
+        background: '#00308717'
     }
 ]
