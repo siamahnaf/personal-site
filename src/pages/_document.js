63252bfc64e530dd802b91ef56e346da50f678d6
Siam Ahnaf <siamahnaf198@gmail.com>
2022-09-05 19:13:16 +0600

    version@12.5.3 (Fixing font issue)

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,7 +10,7 @@ export default class MyDocument extends Document {
                 <Head>
                     <link rel="shortcut icon" href="/favicon.ico" />
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
                     {this.props.emotionStyleTags}
                 </Head>
